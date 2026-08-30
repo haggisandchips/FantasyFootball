d7bf48396cec518ba8b4e50933df0195c7472525
Ivor Potter <ivor.potter@gmail.com>
2026-08-30 15:57:40 +0100

    Latest gradle and JDK 25

--- src/main/java/com/haggisandchips/fantasyfootball/remote/config/RemoteConfig.java
+++ src/main/java/com/haggisandchips/fantasyfootball/remote/config/RemoteConfig.java
@@ -2,11 +2,13 @@ package com.haggisandchips.fantasyfootball.remote.config;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
 public class RemoteConfig {
 
   @Bean
-  private ObjectMapper objectMapper() {
+  ObjectMapper objectMapper() {
 
     return new ObjectMapper();
   }
